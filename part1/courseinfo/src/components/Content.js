41b84d3bbaef71cbdfc4566dbe2634b87309d89f
Dale Bandoni <dalebandoni.10@gmail.com>
2021-04-18 21:42:03 +0100

    exercise 1.2

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Part from './Part'
 
-const Content = ({ part, exercises }) => {
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
   return (
-    <p>
-      {part}
-      <br />
-      {exercises}
-    </p>
+    <div>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
   )
 }
 
