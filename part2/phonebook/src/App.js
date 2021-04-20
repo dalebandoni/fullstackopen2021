ea602fbfc4a8b297b20526e7981d89a41c5359e7
Dale Bandoni <dalebandoni.10@gmail.com>
2021-04-20 16:25:00 +0100

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,14 +3,27 @@ import React, { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = e => {
     e.preventDefault()
     const newPerson = {
       name: newName,
+      number: newNumber,
     }
+
+    const existingName = persons.filter(
+      person => person.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
+    )
+
+    if (existingName[0] !== undefined) {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   console.log(persons)
@@ -27,11 +40,24 @@ const App = () => {
           />
         </div>
         <div>
+          number{' '}
+          <input
+            onChange={({ target }) => setNewNumber(target.value)}
+            value={newNumber}
+          />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))}
+      </ul>
     </div>
   )
 }
