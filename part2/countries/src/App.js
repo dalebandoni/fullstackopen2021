9eb59807889369b6827396e0afe3b223cbcb79a3
Dale Bandoni <dalebandoni.10@gmail.com>
2021-04-21 13:52:10 +0100

    exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  useEffect(() => {
+    const getCountries = async () => {
+      const { data } = await axios.get('https://restcountries.eu/rest/v2/all')
+      setCountries(data)
+    }
+    getCountries()
+  }, [])
+
+  const filteredCountries = countries.filter(country =>
+    country.name.toLowerCase().includes(filter.toLowerCase())
+  )
+  console.log(countries)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries{' '}
+      <input
+        type='text'
+        value={filter}
+        onChange={({ target }) => setFilter(target.value)}
+      />
+      {filteredCountries.length > 10 && (
+        <p>Too many matches, specify another filter</p>
+      )}
+      <ul>
+        {filteredCountries.length < 10 &&
+          filteredCountries.length > 1 &&
+          filteredCountries.map(country => (
+            <p key={country.alpha2Code}>{country.name}</p>
+          ))}
+      </ul>
+      {filteredCountries.length === 1 &&
+        filteredCountries.map(country => (
+          <div key={country.alpha2Code}>
+            <h2>{country.name}</h2>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h3>languages</h3>
+            <ul>
+              {country.languages.map(language => (
+                <li key={language.name}>
+                  {language.name} ({language.nativeName})
+                </li>
+              ))}
+              {console.log('languages are:', country.languages)}
+            </ul>
+            <img
+              src={country.flag}
+              width={'150px'}
+              alt={`flag of ${country.name}`}
+            />
+          </div>
+        ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
